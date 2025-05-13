165bae2156d1685472e63da841a61d69a7c09ede
SpPag <spiridonpagiatis@gmail.com>
2025-05-13 17:26:21 +0300

    feat(project-structure): change a few images, add two dark mode background image options, add the Game type, the GameCard component change the page file a little

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,26 @@
+import { Game } from "@/types/Game";
+
+interface GameCardProps {
+    game?: Game;
+}
+
+const GameCard = ({ game }: GameCardProps) => {
+
+    return (
+        <div className="p-4 border border-2 rounded-xl border-zinc-500 shadow-md dark:border-zinc-600 dark:text-zinc-300">
+            {game ? (
+                <h2>GameCard</h2>
+            ) :
+                <div>
+                    <div>GameCard</div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                </div>
+            }
+
+        </div>
+    )
+}
+
+export { GameCard }
---
+++ src/types/Game.d.ts
@@ -0,0 +1,8 @@
+interface Game {
+    title: string,
+    publisher: string,
+    releaseDate: string,
+    cover: string
+}
+
+export { Game }
