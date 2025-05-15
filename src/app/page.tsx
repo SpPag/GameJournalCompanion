6c27e8b789e58d03a672ba695fce86827cade0dd
SpPag <spiridonpagiatis@gmail.com>
2025-05-15 11:36:50 +0300

    style(dark mode): improve a few dark mode adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { GameCard } from "@/components/GameCard";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="mb-0 flex items-center gap-4 border border-2 rounded-xl border-zinc-500 shadow-xl dark:shadow-zinc-600/45 p-4 dark:border-zinc-900 dark:text-zinc-300">
+      <div className="mb-0 flex items-center gap-4 border border-2 rounded-xl border-zinc-500 shadow-xl p-4 text-zinc-950 dark:shadow-zinc-950/70 dark:border-zinc-900 dark:text-zinc-950 dark:bg-white/10">
         <svg className="h-8 w-8" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" />  <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />  <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />  <line x1="3" y1="6" x2="3" y2="19" />  <line x1="12" y1="6" x2="12" y2="19" />  <line x1="21" y1="6" x2="21" y2="19" />
         </svg>
@@ -18,15 +18,9 @@ export default function Home() {
 
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center dark:text-zinc-300">
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center text-sm text-zinc-950 dark:text-zinc-950 backdrop-blur-xs">
         <p>{"\u00A9"} {new Date().getFullYear()} - Game Journal Companion</p>
       </footer>
     </div>
-//============================================================================ TO DO ============================================================================
-/*
-  - I have two possible dark mode background images in the public folder. Try them and see which is best, delete the other one
-  - I might need to create a tailwind.config.js file and add the dark mode switch there, to which I'll afterward add the dark mode background image
- */
-//============================================================================ TO DO ============================================================================
   );
 }
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,13 +7,13 @@ interface GameCardProps {
 const GameCard = ({ game }: GameCardProps) => {
 
     return (
-        <div className="p-4 border border-2 rounded-xl border-zinc-500 shadow-md dark:border-zinc-600 dark:text-zinc-300">
+        <div className="p-4 border border-2 rounded-xl border-zinc-500 text-2xl text-zinc-950 shadow-md dark:shadow-zinc-950/70 dark:border-zinc-900 dark:text-zinc-950 dark:bg-white/20">
             {game ? (
                 <h2>GameCard</h2>
             ) :
-                <div>
+                <div className="flex flex-col gap-4 items-center">
                     <div>GameCard</div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.2} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                 </div>
