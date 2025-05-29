8e82e842939567d2e9c6fe99cd809f8a4c070a10
SpPag <spiridonpagiatis@gmail.com>
2025-05-29 17:32:06 +0300

    style(ui): Change Toast.tsx to use more fitting bg and text colors, add it the class z-50 to bring it to the forefront while the modal is open

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -17,11 +17,11 @@ const Toast = ({ message, type = "success", onDone, duration = 4000 }: ToastProp
     return () => clearTimeout(id);
   }, [onDone, duration]);
 
-  const bgColor = type === "success" ? "bg-green-600" : "bg-red-600";
+  const bgColor = type === "success" ? "bg-[#19662f]/75 dark:bg-[#143e1e]/60" : "bg-[#882d23] dark:bg-[#741911]/60";
 
   return (
     <div 
-      className={`fixed bottom-6 right-6 ${bgColor} text-white px-4 py-2 rounded shadow-lg`}
+      className={`z-50 fixed bottom-6 right-6 ${bgColor} px-4 py-2 rounded shadow-lg text-[#111827] dark:text-zinc-300`}
     >
       {message}
     </div>
