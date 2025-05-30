3aa565df8bd7d41966c06d649c79f67a1bdf2c08
SpPag <spiridonpagiatis@gmail.com>
2025-05-31 01:34:52 +0300

    refactor: replace the two svgs in the title with the new BookSvg component for less code duplication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { AvailableGamesModal } from "@/components/AvailableGamesModal";
+import { BookSvg } from "@/components/BookSvg";
 import { GameCard } from "@/components/GameCard";
 import { Game } from "@/types/Game";
 import React, { useEffect, useState } from 'react';
@@ -25,13 +26,9 @@ export default function Home() {
   return (
     <div className="relative top-12 sm:top-0 flex flex-col items-center justify-start flex-1 p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex items-center justify-center gap-4 border border-2 rounded-xl w-106 h-16 sm:w-106 sm:h-16 md:w-123 md:h-17 lg:w-141 lg:h-19 text-2xl md:text-3xl lg:text-4xl border-zinc-500 shadow-xl p-4 text-zinc-950 dark:shadow-zinc-950/70 dark:border-zinc-900 dark:text-zinc-950 dark:bg-white/10">
-        <svg className="h-8 w-8" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" />  <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />  <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />  <line x1="3" y1="6" x2="3" y2="19" />  <line x1="12" y1="6" x2="12" y2="19" />  <line x1="21" y1="6" x2="21" y2="19" />
-        </svg>
+        <BookSvg className="h-8 w-8" />
         <h1 className="italic pr-1">Game Journal Companion</h1>
-        <svg className="h-8 w-8" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" />  <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />  <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />  <line x1="3" y1="6" x2="3" y2="19" />  <line x1="12" y1="6" x2="12" y2="19" />  <line x1="21" y1="6" x2="21" y2="19" />
-        </svg>
+        <BookSvg className="h-8 w-8"/>
       </div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
