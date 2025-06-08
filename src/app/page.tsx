50c9eb2fd330e97f2fbc15676bcfd3d281624ea4
SpPag <spiridonpagiatis@gmail.com>
2025-06-08 12:04:06 +0300

    style(ui): extract the welcome user text from the AuthButton in the WelcomeUser.tsx component and call it in the (home)page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { AvailableGamesModal } from "@/components/AvailableGamesModal";
 import { BookSvg } from "@/components/BookSvg";
 import { GameCard } from "@/components/GameCard";
+import { WelcomeUser } from "@/components/WelcomeUser";
 import { Game } from "@/types/Game";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from 'react';
@@ -27,10 +28,13 @@ export default function Home() {
 
   return (
     <div className="relative top-14 flex flex-col items-center justify-start flex-1 p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
-      <div className="flex items-center justify-center gap-4 border border-2 rounded-xl w-106 h-16 sm:w-106 sm:h-16 md:w-123 md:h-17 lg:w-141 lg:h-19 text-2xl md:text-3xl lg:text-4xl border-zinc-500 shadow-xl p-4 text-zinc-950 dark:shadow-zinc-950/70 dark:border-zinc-900 dark:text-zinc-950 dark:bg-white/10">
-        <BookSvg className="h-8 w-8" />
-        <h1 className="italic pr-1">Game Journal Companion</h1>
-        <BookSvg className="h-8 w-8" />
+      <div>
+        <div className="flex items-center justify-center gap-4 border border-2 rounded-xl w-106 h-16 sm:w-106 sm:h-16 md:w-123 md:h-17 lg:w-141 lg:h-19 text-2xl md:text-3xl lg:text-4xl border-zinc-500 shadow-xl p-4 text-zinc-950 dark:shadow-zinc-950/70 dark:border-zinc-900 dark:text-zinc-950 dark:bg-white/10">
+          <BookSvg className="h-8 w-8" />
+          <h1 className="italic pr-1">Game Journal Companion</h1>
+          <BookSvg className="h-8 w-8" />
+        </div>
+        <WelcomeUser />
       </div>
       <main className="flex flex-col row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
---
+++ src/components/WelcomeUser.tsx
@@ -0,0 +1,22 @@
+import { useSession } from "next-auth/react";
+
+
+const WelcomeUser = () => {
+
+    const { data: session, status } = useSession();
+
+    // checking for status == loading to avoid flickering the div before the session data is available
+    if (status === "loading") return null;
+
+    return (
+        <>
+            {session &&
+                <div className="border border-2 rounded-xl z-10 px-4 py-2 text-base sm:text-xl border-zinc-500 shadow-xl text-zinc-950 dark:shadow-zinc-950/70 dark:border-zinc-900 dark:text-zinc-950 dark:bg-white/10 m-auto w-fit">
+                    Welcome {session.user.username}
+                </div>
+            }
+        </>
+    )
+};
+
+export { WelcomeUser };
