8e76680fbde6d28b28095560ea5015c660013fe9
SpPag <spiridonpagiatis@gmail.com>
2025-06-19 11:18:24 +0300

    style(ui): added background color change on focus for light mode and restructured the className a bit for readability

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -16,7 +16,17 @@ const AuthButton = () => {
     return (
         <button
             onClick={() => session ? signOut({ callbackUrl: "/" }) : router.push("/login")} // can add '{ callbackUrl: "/login" }' as the callback URL in signOut(), to redirect to the login page upon logout
-            className='flex items-center z-50 border px-4 py-2 rounded absolute top-0 right-0 mt-5 mr-5 text-[0.95rem]/[1.25rem] md:text-[0.97rem]/[1.3rem] lg:text-base bg-[#c59854] text-[#111827] border-stone-700 focus:outline-2 focus:outline-[#867162] hover:bg-[#b68945] active:bg-[#ad803c] dark:bg-neutral-600 dark:hover:bg-[#4b4b4b] dark:focus:bg-[#4b4b4b] dark:active:bg-[#393939] dark:text-zinc-300 dark:focus:outline-2 dark:focus:outline-zinc-500'
+            className='
+            flex items-center z-50 px-4 py-2
+            mt-5 mr-5
+            absolute top-0 right-0
+            border rounded border-stone-700 
+            text-[0.95rem]/[1.25rem] md:text-[0.97rem]/[1.3rem] lg:text-base
+            text-[#111827] dark:text-zinc-300
+            bg-[#c59854] dark:bg-neutral-600
+            focus:bg-[#a37732] focus:outline-2 focus:outline-[#867162] dark:focus:bg-[#4b4b4b] dark:focus:outline-2 dark:focus:outline-zinc-500
+            hover:bg-[#b68945] dark:hover:bg-[#4b4b4b]
+            active:bg-[#ad803c] dark:active:bg-[#393939]'
         >
             {session ? "Logout" : "Login"}
         </button>
