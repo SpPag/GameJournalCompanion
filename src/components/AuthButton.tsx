8dc8bc6a6af125216766652e7498ec061f5bcd64
SpPag <spiridonpagiatis@gmail.com>
2025-06-19 17:16:57 +0300

    style(ui): make the Register and Auth buttons more round, darker in color and have a dark-specific border

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -20,10 +20,10 @@ const AuthButton = () => {
             flex items-center z-50 px-4 py-2
             mt-5 mr-5
             absolute top-0 right-0
-            border rounded border-stone-700 
+            border rounded-lg border-stone-700 dark:border-neutral-600
             text-[0.95rem]/[1.25rem] md:text-[0.97rem]/[1.3rem] lg:text-base
             text-[#111827] dark:text-zinc-300
-            bg-[#c59854] dark:bg-neutral-600
+            bg-[#c59854] dark:bg-neutral-800
             focus:bg-[#a37732] focus:outline-2 focus:outline-[#867162] dark:focus:bg-[#4b4b4b] dark:focus:outline-2 dark:focus:outline-zinc-500
             hover:bg-[#b68945] dark:hover:bg-[#4b4b4b]
             active:bg-[#ad803c] dark:active:bg-[#393939]"
--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -17,7 +17,17 @@ const RegisterButton = () => {
             {status === "unauthenticated" && (
                 <button
                     onClick={() => router.push("/register")}
-                    className='flex items-center z-50 border px-4 py-2 rounded absolute top-0 right-22 mt-5 mr-2 sm:mr-3 md:mr-4 lg:mr-5 bg-[#c59854] text-[#111827] border-stone-700 focus:outline-2 focus:outline-[#867162] hover:bg-[#b68945] active:bg-[#ad803c] dark:bg-neutral-600 dark:hover:bg-[#4b4b4b] dark:focus:bg-[#4b4b4b] dark:active:bg-[#393939] dark:text-zinc-300 dark:focus:outline-2 dark:focus:outline-zinc-500'
+                    className="
+                    flex items-center z-50 px-4 py-2
+                    mt-5 mr-2 sm:mr-3 md:mr-4 lg:mr-5
+                    absolute top-0 right-22 mt-5 mr-2 sm:mr-3 md:mr-4 lg:mr-5
+                    border rounded-lg border-stone-700 dark:border-neutral-600
+                    text-[0.95rem]/[1.25rem] md:text-[0.97rem]/[1.3rem] lg:text-base
+                    text-[#111827] dark:text-zinc-300
+                    bg-[#c59854] dark:bg-neutral-800
+                    focus:bg-[#a37732] focus:outline-2 focus:outline-[#867162] dark:focus:bg-[#4b4b4b] dark:focus:outline-2 dark:focus:outline-zinc-500
+                    hover:bg-[#b68945] dark:hover:bg-[#4b4b4b]
+                    active:bg-[#ad803c] dark:active:bg-[#393939]"
                 >
                     Register
                 </button>
