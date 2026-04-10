99eee055e24aea67662a9c9b6c6ec3d1104df390
SpPag <spiridonpagiatis@gmail.com>
2026-04-11 01:03:30 +0300

    move the src/app/api/register/route.ts route into the src/app/api/account/ directory for better organization / grouping

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -18,7 +18,7 @@ const RegisterPage = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const res = await fetch("/api/register", {
+    const res = await fetch("/api/account/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, username, password }),
